24c257f1b0f89ab6b607f9c971cf1776432b0bf3
Keiji-morita <keijim3116@gmail.com>
2022-07-08 14:31:56 +0900

    リスト切り出し

---
+++ lib/domain/note/adding.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:practiceddd/domain/note/todo_class.dart';
+
+final _todocontroller = TextEditingController();
+
+void addingtodo() {
+          todos.add(_todocontroller.text);
+      }
+      
---
+++ lib/domain/note/todo_class.dart
@@ -0,0 +1,17 @@
+
+class todo {
+  int id;
+
+  String title;
+
+  bool isDone;
+
+  todo(
+      this.id,
+      this.title,
+      this.isDone,
+  );
+}
+
+final List<String> todos = <String>['A', 'B', 'C'];
+
